a6d15036eef8c09f084c6c0a66d94c1c3f6393ac
mac <1053210246@qq.com>
2018-12-20 21:12:35 +0800

    1 remove setVertsDirty 2 add Camera adapter

--- engine/assemblers/sprite/simple.js
+++ engine/assemblers/sprite/simple.js
@@ -60,7 +60,6 @@ cc.Sprite._assembler.simple = {
         if (frame && sprite._vertsDirty) {
             this.updateVerts(sprite);
             sprite._vertsDirty = false;
-            sprite._renderHandle.setVertsDirty();
         }
     },
 

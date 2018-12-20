a00a1b61eab8d46775c791d06160062e92370e72
caryliu <caryliu1999@gmail.com>
2018-12-20 14:20:54 +0800

    Add native-renderer mask.

---
+++ engine/assemblers/mask-assembler.js
@@ -0,0 +1,67 @@
+/****************************************************************************
+ Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
+
+ https://www.cocos.com/
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated engine source code (the "Software"), a limited,
+ worldwide, royalty-free, non-assignable, revocable and non-exclusive license
+ to use Cocos Creator solely to develop games on your target platforms. You shall
+ not use Cocos Creator software for developing other software or tools that's
+ used for developing games. You are not granted to publish, distribute,
+ sublicense, and/or sell copies of Cocos Creator.
+
+ The software or tools in this License Agreement are licensed, not sold.
+ Xiamen Yaji Software Co., Ltd. reserves all rights not expressly granted to you.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ ****************************************************************************/
+
+const Mask = cc.Mask;
+const RenderFlow = cc.RenderFlow;
+const spriteAssembler = cc.Sprite._assembler.simple;
+const graphicsAssembler = cc.Graphics._assembler;
+
+cc.Mask._assembler = {
+    updateRenderData (mask) {
+        if (!mask._renderData) {
+            // Update clear graphics material
+            graphicsAssembler.updateRenderData(mask._clearGraphics);
+
+            if (mask._type === Mask.Type.IMAGE_STENCIL) {
+                mask._renderData = spriteAssembler.createData(mask);
+            }
+        }
+        let renderData = mask._renderData;
+        if (mask._type === Mask.Type.IMAGE_STENCIL) {
+            if (mask.spriteFrame) {
+                if (!mask._material) {
+                    mask._activateMaterial();
+                }
+                spriteAssembler.updateRenderData(mask);
+                renderData._material = mask._material;
+            }
+            else {
+                mask._material = null;
+            }
+            mask._renderHandle.setMaskType(true);
+        }
+        else {
+            if (!mask._material) {
+                mask._activateMaterial();
+            }
+            mask._graphics._material = mask._material;
+            graphicsAssembler.updateRenderData(mask._graphics);
+            mask._renderHandle.setMaskType(false);
+        }
+        mask._renderHandle.updateMaterial(0, mask._material);
+        mask._renderHandle.setMaskInverted(mask.inverted);
+        mask._renderHandle.setUseModel(mask._type !== Mask.Type.IMAGE_STENCIL);
+    },
+};
